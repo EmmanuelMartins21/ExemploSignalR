3c16192b09cb5b0bda50288ebc6c912ba14e4a5c
marti <marti@DESKTOP-1OB9IHH>
2021-07-12 14:31:58 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploSignalR/ExemploHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExemploSignalR
+{
+    public class ExemploHub : Hub
+    {
+        public void EnviarMensagemParaHub(string msg)
+        {
+            Clients.All.enviarMensagemParaClient(msg);
+        }
+    }
+}
---
+++ ExemploSignalR/Startup1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(ExemploSignalR.Startup1))]
+
+namespace ExemploSignalR
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR(); // cria uma rota para chamar o serviço
+        }
+    }
+}
